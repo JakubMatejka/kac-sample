e60c47596c6640f83d90c7d03704453738817e8c
Jakub Matějka <jakub@keboola.com>
2021-11-16 16:59:19 +0100

    Init (with v1)

--- main/_shared/keboola.snowflake-transformation/codes/7558782-join/code.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE "address_city" AS
-SELECT "a"."name", "a"."surname", "c"."LatD", "c"."LonD"
-FROM "addresses" "a"
-LEFT JOIN "cities" "c" ON ("a"."city" = "c"."City")
-WHERE "a"."state"={{state}}
